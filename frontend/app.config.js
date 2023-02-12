727b73f222b1539853a19956d69d12d7834cf4f0
Magnar Dæhli <magnard44@gmail.com>
2023-02-12 22:55:22 +0100

    added static fetch url

--- frontend/app.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: "UKA_test",
-  version: "1.0.0",
-  extra: {
-    apiUrl: process.env.API_URL,
-  },
-};
--- frontend/screens/HomeScreen.tsx
+++ frontend/screens/HomeScreen.tsx
@@ -1,13 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-import Constants from "expo-constants";
-
-import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config();
-
 const HomeScreen = () => {
-  console.log(process.env.API_URL);
   return (
     <View style={styles.container}>
       <Text style={styles.text}>FETESTE APPEN EVER</Text>
